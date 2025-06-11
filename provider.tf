6967f3e17495b968e2a4951323e5f60d4462365e
swl <09soewailynn@gmail.com>
2025-06-11 22:47:36 +0700

    remove profile for credential

--- provider.tf
+++ provider.tf
@@ -9,5 +9,5 @@ terraform {
 
 provider "aws" {
   region  = var.aws_region
-  profile = var.aws_profile # Optional: if using a named profile from ~/.aws/credentials
+  # profile = var.aws_profile # Optional: if using a named profile from ~/.aws/credentials
 }
